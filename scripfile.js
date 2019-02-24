b885159434b8c339cfc959b5db3645afa66f834a
AllianceMbonigba <alliancemboni@gmail.com>
2019-02-24 14:39:58 +0200

    webdevass2

--- scripfile.js
+++ scripfile.js
@@ -2,7 +2,7 @@
 
 let i = 0;
 
-function imageChanger() {
+function slideChanger() {
     let images = []; // array of images
     // initiate image at this potion
     images[0] = 'header.jpg';
@@ -17,9 +17,40 @@ function imageChanger() {
     }
 
 
-    setTimeout("imageChanger()", 3000);
+    setTimeout("slideChanger()", 3000);
+}
+window.onload = slideChanger;
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
 
 
-window.onload = imageChanger;
